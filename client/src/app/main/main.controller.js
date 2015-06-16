dbd2e17b3fd39eb69e11814064c3e360a4d0674d
jorge_arale <naewots@gmail.com>
2015-06-16 17:59:40 -0300

    svg de prueba y directivas

--- client/src/app/main/main.controller.js
+++ client/src/app/main/main.controller.js
@@ -1,7 +1,21 @@
 'use strict';
 
 angular.module('smokeWebClient')
-  .controller('MainCtrl', function ($scope) {
-
-  
-  });
+.controller('MainCtrl', function ($scope) {
+    $scope.thermometer = {
+        value: '100'
+    }
+    $scope.smokeLevel = {
+        value: '50'
+    }    
+  })
+.directive('ngThermometer', function(){
+    return {
+        templateUrl: '../app/directives/thermometer.html'
+    }
+})
+.directive('ngSmokeLevel', function(){
+    return {
+        templateUrl: '../app/directives/smokeLevel.html'
+    }
+});
