05d46917c4ddfae7d6c07b45645a910b5b53f578
Zacarias F. Ojeda <zojeda@gmail.com>
2015-08-19 16:21:11 -0300

    fixing serialReader

--- data_reader/serialReader.js
+++ data_reader/serialReader.js
@@ -1,13 +1,18 @@
 'use strict';
 
+var serialport =  require("serialport");
+
 module.exports = function(port) {
-  var SerialPort = require("serialport").SerialPort;
-  var serialPort = new SerialPort(port);
+  var SerialPort = serialport.SerialPort;
+  var serialPort = new SerialPort(port, {
+    parser: serialport.parsers.readline("\n"),
+    baudrate: 9600
+  });
 
   return {
     startRead: function(onDataCb) {
       serialPort.on('open', function() {
-        console.log('Serial Port Opend');
+        console.log('Serial Port Opened');
         serialPort.on('data', function(data) {
           onDataCb(data.toString("ascii"));
         });
